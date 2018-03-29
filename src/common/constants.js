af855cce7ec3a5efd8388a41dc77ec67db80ca7a
Stavros Angelis <stavrosangelis@gmail.com>
2018-03-29 17:07:24 +0100

    ~minor update

--- src/common/constants.js
+++ src/common/constants.js
@@ -1,4 +1,4 @@
-export const APIPath = "http://localhost:8008/api/";
-export const WPRestPath = "http://localhost:8000/wp-json/wp/v2/";
+export const APIPath = "http://xl1916apps.mucampus.ie:8008/api/";
+export const WPRestPath = "http://xl1916apps.mucampus.ie:8000/wp-json/wp/v2/";
 export const basename = "/ui/";
 //export const basename = "/diyhistory-react/my-app/build/";
